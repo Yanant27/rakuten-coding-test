99aef057e0b2760fc55ee74a308f29dbe211f350
Yanant27 <htooyanant.hyk@gmail.com>
2022-10-11 23:06:09 +0630

    Initial Commit.

---
+++ coding-test/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+	public int solution(int[] A) {
+    	int binarian = 0;
+        for (int i = 0; i < A.length; i++) {
+			binarian += Math.pow(2, A[i]);
+		}
+        
+        List<Integer> shortest = new ArrayList();
+        while (binarian > 0) {
+        	int temp = (int) log2(binarian);
+//        	 System.out.println(temp);
+        	shortest.add(temp);
+        	binarian -= Math.pow(2, temp);
+        }
+		return shortest.size();
+    }
+
+    private double log2(int x) {
+        return Math.log(x) / Math.log(2);
+    }
+    
+    public static void main(String[] args) {
+		System.out.println(new Solution().solution(new int[] {1,0,2,0,0,2}));
+		System.out.println(new Solution().solution(new int[] {1,5,4}));
+	}
+}
